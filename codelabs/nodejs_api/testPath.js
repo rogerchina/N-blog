74153fcfcd15be9f09ec8f923b0b804d5e3fbec8
roger.zhang <zhangyonghuifang@163.com>
2017-04-15 11:25:29 +0800

    update

--- codelabs/nodejs_api/testPath.js
+++ codelabs/nodejs_api/testPath.js
@@ -7,6 +7,12 @@ var path = require('path');
 
 function main() {
     console.log(path.isAbsolute('/opt'));
+
+    console.log(path.basename('/home/roger/test/test.txt'));
+
+    console.log(path.posix.basename('/home/roger/test/test.txt'));
+
+    console.log(path.basename('/tmp/test/text.txt', '.txt'));
 }
 
 main();
