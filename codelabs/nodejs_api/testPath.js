f5acf8f6528ec1d628d60693b053570ddd43c9af
roger.zhang <zhangyonghuifang@163.com>
2017-04-15 16:06:51 +0800

    update

--- codelabs/nodejs_api/testPath.js
+++ codelabs/nodejs_api/testPath.js
@@ -13,6 +13,40 @@ function main() {
     console.log(path.posix.basename('/home/roger/test/test.txt'));
 
     console.log(path.basename('/tmp/test/text.txt', '.txt'));
+
+    var p = process.env.PATH;
+    console.log(p);
+    console.log('path.win32.delimiter:' + path.win32.delimiter);
+    console.log('path.posix.delimiter:' + path.posix.delimiter);
+    console.log('path.delimiter:' + path.delimiter);
+    console.log('PATH.split:' + p.split(path.delimiter));
+
+    console.log('path.dirname:' + path.dirname('/tmp/test/test.txt'));
+    console.log('path.dirname:' + path.dirname('/tmp/test/'));
+    console.log('path.dirname:' + path.dirname('/tx/'));
+    //console.log('path.dirname:' + path.dirname([]));
+
+    console.log(path.extname('index.html')); // Returns: '.html'
+    console.log(path.extname('index.coffee.md')); // Returns: '.md'
+    console.log(path.extname('index.')); // Returns: '.'
+    console.log(path.extname('index')); // Returns: ''
+    console.log(path.extname('.index')); // Returns: ''
+
+    console.log(path.isAbsolute('/foo/bar')); // true
+    console.log(path.isAbsolute('/baz/..'));  // true
+    console.log(path.isAbsolute('qux/'));     // false
+    console.log(path.isAbsolute('.'));  // false
+
+    console.log(path.join('/foo', 'bar', 'baz/asdf', 'quux', '..')); // Returns: '/foo/bar/baz/asdf'
+    console.log(path.join('/foo', 'bar', 'baz/asdf', 'quux')); // Returns: '/foo/bar/baz/asdf/quux'
+    console.log(path.join('/foo', 'bar', 'baz/asdf', 'quux', '.')); // Returns: '/foo/bar/baz/asdf'
+    //console.log(path.join('foo', {}, 'bar'));// throws TypeError: Arguments to path.join must be strings
+
+    console.log(path.normalize('/home/roger'));
+    console.log(path.normalize('/home//roger/test/..'));
+    console.log(path.normalize('/home/roger/test/\.'));
+
+    console.log(path.parse('/home/roger/test/test.txt'));
 }
 
 main();
