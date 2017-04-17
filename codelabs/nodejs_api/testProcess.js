52ff541ee5368f72f00549cfd13fdf469b34ca16
roger.zhang <zhangyonghuifang@163.com>
2017-04-18 07:58:39 +0800

    update

---
+++ codelabs/nodejs_api/testProcess.js
@@ -0,0 +1,17 @@
+/**
+ * Created by roger on 17-4-18.
+ */
+
+// process object is global, no need to use require() to import
+
+
+
+function main() {
+    // process.
+}
+
+function print(str) {
+    console.log(str);
+}
+
+main();
