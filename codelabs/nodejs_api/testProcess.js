17f0fb10b15e0a223edcf2079d362c9cff5a2b17
roger.zhang <zhangyonghuifang@163.com>
2017-04-27 07:52:38 +0800

    update

--- codelabs/nodejs_api/testProcess.js
+++ codelabs/nodejs_api/testProcess.js
@@ -4,11 +4,34 @@
 
 // process object is global, no need to use require() to import
 
+var fs = require('fs')
 
 
 function main() {
-    testExit();
+    //testExit();
+    testUncaughtException();
+}
 
+function testUncaughtException() {
+    process.on('uncaughtException', function (err) {
+        fs.writeSync(1, 'Caught exception: ${err}');
+    })
+    
+    setTimeout(function () {
+        print('this will run...');
+    }, 500);
+
+    // Intentionally cause a exception, but don't catch it.
+    nonexistentFunction();
+    print('this will not run.')
+}
+
+function testRejectionHandle() {
+    //TODO need to do it after study Promise
+}
+
+function testMessage() {
+    //TODO need to do it after study cluster
 }
 
 function testExit() {
@@ -19,6 +42,13 @@ function testExit() {
     process.on('exit', function (code) {
         print('i am exiting... with exit code is ' + code);
     })
+
+    process.on('exit', (code) => {
+        print('test');
+        setTimeout(() => {
+            console.log('This will not run');
+        }, 0);
+    });
 }
 
 function print(str) {
